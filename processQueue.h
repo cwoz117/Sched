d711bb7bcfbbc47fec6cd0c1205b9e9058eb78f9
cwoz117 <cwoz117@hotmail.ca>
2015-06-18 10:04:55 -0600

    must test processQueue, added schedConfig.h

--- processQueue.h
+++ processQueue.h
@@ -1,33 +1,50 @@
-/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
- *                    processQueue.h                       *
- *                                                         *
- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *                        processQueue.h                             *
+ *                                                                   *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 #include <pthread.h>
 
 
 struct processNode {
   p_thread proc;
-  struct processNode next;
+  struct processNode *next;
 };
 
-processNode head;
-processNode tail;
+extern processNode *head;
+extern processNode *tail;
 
 
-void addProcess (p_thread &p){
-
-
+void addProcess (p_thread p){
+  if (head == NULL){
+    struct processNode tmp;
+    tmp =(struct processNode *)malloc(sizeof(struct processNode));
+    tmp.proc = p
+    head = &tmp;
+    head->next = NULL;
+    tail = head;
+  } else {
+    struct processNode tmp;
+    tmp = (struct processNode *)malloc(sizeof(struct processNode));
+    tmp.proc = p;
+    tail.next = tmp;
+    tail = tail.next;
+  }
 }
 
 processNode nextProcess(){
   processNode tmp = head;
-
-
+  head = head.next;
+ 
+  if (tmp == NULL){
+    return NULL;
+  }else {
+    return tmp;
+  }
 }
 
-/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
- END OF FILE END OF FILE END OF FILE END OF FILE END OF FILE
- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *END OF FILE END OF FILE END OF FILE END OF FILE END OF FILE END OF *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
 
 
