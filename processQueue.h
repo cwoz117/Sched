aa647659120f37757cea05406929e48acf821c85
cwoz117 <cwoz117@hotmail.ca>
2015-06-18 09:35:27 -0600

    Set up process skeleton

--- processQueue.h
+++ processQueue.h
@@ -2,9 +2,28 @@
  *                    processQueue.h                       *
  *                                                         *
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+#include <pthread.h>
 
 
+struct processNode {
+  p_thread proc;
+  struct processNode next;
+};
 
+processNode head;
+processNode tail;
+
+
+void addProcess (p_thread &p){
+
+
+}
+
+processNode nextProcess(){
+  processNode tmp = head;
+
+
+}
 
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
  END OF FILE END OF FILE END OF FILE END OF FILE END OF FILE
